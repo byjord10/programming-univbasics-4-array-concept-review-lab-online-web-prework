a95e92075133fe82d6519ee1d31714ede1abdb24
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-16 01:52:27 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,23 @@
+require 'spec_helper'
+require 'pry'
+
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+array.index(value_to_find)
 end
+# scale = [3, 4,1,2,7,9,6]
+# amazing = [1]
+ # variableImSaving = find_element_index(amazing, 6)
+
 
 def find_max_value(array)
+  array.max
+
+
   # Add your solution here
 end
 
 def find_min_value(array)
+  array.min
   # Add your solution here
 end
